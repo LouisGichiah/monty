cda33c744ba66efd73f26cfce9ead6de4d5caa11
lindamukami <lindamukami32@gmail.com>
2022-06-18 10:45:54 +0000

    betty resolved

--- opcode-odd.c
+++ opcode-odd.c
@@ -9,8 +9,8 @@
 void pint(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp = *stack;
-	
-	if(tmp == NULL)
+
+	if (tmp == NULL)
 	{
 		dprintf(STDERR_FILENO, "L%d: can't pint, stack empty\n", line_number);
 		freer();
