b62cf5b3dcc8e2ad8e6b35d7d34ed74f488ffc2e
lindamukami <lindamukami32@gmail.com>
2022-06-18 11:10:40 +0000

    task 1

--- pint.c
+++ pint.c
@@ -9,13 +9,11 @@ void pint(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp = *stack;
 
-	if (tmp == NULL)
+	if (!tmp)
 	{
 		dprintf(STDERR_FILENO, "L%d: can't pint, stack empty\n", line_number);
 		freer();
 		exit(EXIT_FAILURE);
 	}
-	while (tmp->next != NULL)
-		tmp = tmp->next;
 	printf("%d\n", tmp->n);
 }
