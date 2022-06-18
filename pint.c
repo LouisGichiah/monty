b4635353f0150bb34e3b2aae4bb92203c3da8153
lindamukami <lindamukami32@gmail.com>
2022-06-18 11:20:14 +0000

    added pint

--- pint.c
+++ pint.c
@@ -2,18 +2,20 @@
 /**
  *pint - prints the top value of a stack
  *@stack:pointer to stack
- *@ln:line number
+ *@line_number:line number
  *Return:void
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp = *stack;
 
-	if (!tmp)
+	if (tmp == NULL)
 	{
 		dprintf(STDERR_FILENO, "L%d: can't pint, stack empty\n", line_number);
 		freer();
 		exit(EXIT_FAILURE);
 	}
+	while (tmp->next != NULL)
+		tmp = tmp->next;
 	printf("%d\n", tmp->n);
 }
